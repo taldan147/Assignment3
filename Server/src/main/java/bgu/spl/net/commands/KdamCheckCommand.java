8707764ececca206a82297f87472741081f5dfb5
idocarmi2205 <idocarmi2205@gmail.com>
2021-01-03 00:05:00 +0200

    added KdamCheckCommand

---
+++ Server/src/main/java/bgu/spl/net/commands/KdamCheckCommand.java
@@ -0,0 +1,24 @@
+package bgu.spl.net.commands;
+
+import bgu.spl.net.commands.base.QueryMessage;
+import bgu.spl.net.common.Course;
+import bgu.spl.net.srv.Database;
+
+import java.io.Serializable;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class KdamCheckCommand extends QueryMessage {
+    protected KdamCheckCommand(short opcode, short queryNumber) {
+        super(opcode, queryNumber);
+    }
+
+    @Override
+    public Serializable execute(Database arg) {
+        return new Ack(opcode,arg.getCourse(query)
+                .getKdamCourses().stream()
+                .map(course
+                        -> String.valueOf(course.getCourseNum()))
+                .collect(Collectors.toList()));
+    }
+}
